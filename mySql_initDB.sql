35841d1303b4366840695b3b3063741f696bc637
Vadim-Havrylitse <vfgavrilice@gmail.com>
2022-05-29 00:13:57 +0300

    update

--- mySql_initDB.sql
+++ mySql_initDB.sql
@@ -1,30 +1,35 @@
 CREATE DATABASE testgoitdb;
 
-CREATE TABLE testgoitdb.developers (
+CREATE TABLE developers (
 	id bigint AUTO_INCREMENT,
     name varchar(20) NOT NULL,
     surname varchar(20) NOT NULL,
     age bigint CHECK (age>0),
     sex varchar(10),
+    companies_id bigint,
+    salary int,
     PRIMARY KEY(id)
     );
     
-CREATE TABLE testGoITDB.skills (
+CREATE TABLE skills (
 	id bigint AUTO_INCREMENT,
     industry varchar(20),
     degree varchar(20) DEFAULT 'Junior',
     PRIMARY KEY(id)
     );
 
-CREATE TABLE testgoitdb.projects (
+CREATE TABLE projects (
 	id bigint AUTO_INCREMENT,
     name varchar(50) UNIQUE,
     budget bigint NOT NULL, 
     release_date date,
+    companies_id bigint NOT NULL,
+    customers_id bigint NOT NULL,
+    cost bigint not NULL,
     PRIMARY KEY(id)
     );
     
-CREATE TABLE testgoitdb.companies (
+CREATE TABLE companies (
 	id bigint AUTO_INCREMENT,
     name varchar(50) NOT NULL,
     country varchar(20),
@@ -32,39 +37,33 @@ CREATE TABLE testgoitdb.companies (
     PRIMARY KEY(id)
     );
     
-CREATE TABLE testgoitdb.customers (
+CREATE TABLE customers (
 	id bigint AUTO_INCREMENT,
     name varchar(20),
     surname varchar(20),
     PRIMARY KEY(id)
     );
 
-CREATE TABLE testgoitdb.developers_projects (
+CREATE TABLE developers_projects (
 	developers_id bigint,
     projects_id bigint,
-    FOREIGN KEY (projects_id) REFERENCES testgoitdb.projects(id),
-    FOREIGN KEY (developers_id) REFERENCES testgoitdb.developers(id)
+    FOREIGN KEY (projects_id) REFERENCES projects(id),
+    FOREIGN KEY (developers_id) REFERENCES developers(id)
     );
     
-CREATE TABLE testgoitdb.developers_skills (
+CREATE TABLE developers_skills (
 	developers_id bigint,
     skills_id bigint,
-    FOREIGN KEY (skills_id) REFERENCES testgoitdb.skills(id),
-    FOREIGN KEY (developers_id) REFERENCES testgoitdb.developers(id)
+    FOREIGN KEY (skills_id) REFERENCES skills(id),
+    FOREIGN KEY (developers_id) REFERENCES developers(id)
     );
-    
-ALTER TABLE testgoitdb.projects
-	ADD COLUMN companies_id bigint NOT NULL,
-    ADD COLUMN customers_id bigint NOT NULL,
-    ADD FOREIGN KEY (customers_id) REFERENCES testgoitdb.customers(id),
-    ADD FOREIGN KEY (companies_id) REFERENCES testgoitdb.companies(id);
-    
-ALTER TABLE testgoitdb.developers
-	ADD COLUMN companies_id bigint,
-    ADD FOREIGN KEY (companies_id) REFERENCES testgoitdb.companies(id);
-
-ALTER TABLE testgoitdb.developers
-    ADD COLUMN salary int;
 
 ALTER TABLE projects
-    ADD COLUMN cost bigint not NULL;
+    ADD FOREIGN KEY (customers_id) REFERENCES customers(id);
+
+ALTER TABLE projects
+    ADD FOREIGN KEY (companies_id) REFERENCES companies(id);
+
+ALTER TABLE developers
+    ADD FOREIGN KEY (companies_id) REFERENCES companies(id);
+
--- mySql_populateDB.sql
+++ mySql_populateDB.sql
@@ -1,40 +1,40 @@
-INSERT INTO testgoitdb.companies (name,country,city)
+INSERT INTO companies (name,country,city)
 	VALUES
 	('ABN inc', 'Makedonia', 'Tringo'),
 	('FIFA corp', 'Italy', 'Rome'),
 	('Spring4beavers', 'Holland', 'Amsterdam');
 
-INSERT INTO testgoitdb.customers (name, surname)
+INSERT INTO customers (name, surname)
 	VALUES
 	('John', 'Makedon'),
 	('Bob', 'Fridgh'),
 	('Ivan', 'Petrenko');
 
-INSERT INTO testgoitdb.skills (industry, degree)
+INSERT INTO skills (industry, degree)
 	VALUES
 	('Java', 'Middle'),
     ('JS', 'Middle'),
     ('C++', 'Senior'),
     ('C++', 'Middle');
     
-INSERT INTO testgoitdb.skills (industry)
+INSERT INTO skills (industry)
 	VALUES
 	('Java'),
     ('JS');
     
-INSERT INTO testgoitdb.projects 
+INSERT INTO projects
 	VALUES
-    (1, 'Loombook', 123000, '2022-12-22', 1,2),
-    (2, 'IDEA Workspace', 2340000500, null, 2,2),
-    (3, 'Git workbench', 450100500, null, 1,3);
+    (1, 'Loombook', 123000, '2022-12-22', 1, 2, 230000),
+    (2, 'IDEA Workspace', 2340000500, null, 2, 2, 12500),
+    (3, 'Git workbench', 450100500, null, 1, 3, 12500);
     
-INSERT INTO testgoitdb.developers
+INSERT INTO developers
 	VAlUES
-    (1, 'Nenci', 'Ibragimovich', 31, 'female', 2),
-    (2, 'Victor', 'Ronaldo', 29, 'male', null),
-    (3, 'Imanuel', 'Mikron', 22, 'other', 1);
+    (1, 'Nenci', 'Ibragimovich', 31, 'female', 2, 12000),
+    (2, 'Victor', 'Ronaldo', 29, 'male', null, 22000),
+    (3, 'Imanuel', 'Mikron', 22, 'other', 1, 12000);
     
-INSERT INTO testgoitdb.developers_skills
+INSERT INTO developers_skills
 	VALUES
     (2,3),
     (1,3),
@@ -44,7 +44,7 @@ INSERT INTO testgoitdb.developers_skills
     (3,1);
     
     
-INSERT INTO testgoitdb.developers_projects
+INSERT INTO developers_projects
 	VALUES 
     (1, 2),
     (2, 2),
@@ -52,27 +52,3 @@ INSERT INTO testgoitdb.developers_projects
     (3, 2),
     (1, 1),
 	(1, 3);
-
-UPDATE testgoitdb.developers
-SET salary = 12000
-WHERE id = 1;
-
-UPDATE testgoitdb.developers
-SET salary = 22000
-WHERE id = 2;
-
-UPDATE testgoitdb.developers
-SET salary = 12000
-WHERE id = 3;
-
-UPDATE projects
-SET cost = 12500
-WHERE id = 2;
-
-UPDATE projects
-SET cost = 230000
-WHERE id = 1;
-
-UPDATE projects
-SET cost = 12500
-WHERE id = 3;
